e6f00a7b1d8a24b7716cd45813b9490f7bea7695
HALO444 <madhushanka.halo44o@gmail.com>
2025-07-04 10:49:43 +0530

    pointers

--- chapter12/main.c
+++ chapter12/main.c
@@ -3,7 +3,14 @@
 int main(void)
 {
     int num[] = {1,2,3,4};  
-    int *p = num;
-    printf("%d", *(p+1));
+
+    int* p;
+    printf("%p\n",&p);
+    p = num;
+    printf("%d\n",*p);
+    printf("%p\n", &num);
+    printf("%p\n", &num[0]);
+    //int *p = num;
+    //printf("%d", *(p+1));
    
 }
