36416d70f55ee139a3ac87571d5dd9a4e3abf5f8
Madhushanka <152682129+HALO444@users.noreply.github.com>
2026-03-22 09:10:24 +0000

    uninizialized

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 
 int main(){
-  std::cout << "Enter your number: ";
-  int number{};
-  std::cin >> number;
-  std::cout << "Number that you entered is: "<< number <<"\n";
-  std::cout << "Enter two Numbers1: ";
-  int num1{};
-  int num2{};
-  std::cin >>num1 >>num2;
-  std::cout << "Print Numbers "<<num1 << " " <<num2 << "\n";
-  std::cout <<"Enter two Numbers2: ";
-  std::cin >> num1;
-  std::cin >> num2;
-  std::cout << "Print Numbers "<<num1 << " " <<num2 << "\n";
+  int x;
+  std::cout <<x;
 }
